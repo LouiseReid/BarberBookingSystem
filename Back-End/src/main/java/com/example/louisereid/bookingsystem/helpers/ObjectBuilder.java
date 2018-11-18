fea962161e8fb587e8b1a1959cd5c74b4bfcbb4a
Louise Reid <l_reid@hotmail.co.uk>
2018-11-18 14:44:47 +0000

    rendering dynamic data on timeline - issue with date format

--- Back-End/src/main/java/com/example/louisereid/bookingsystem/helpers/ObjectBuilder.java
+++ Back-End/src/main/java/com/example/louisereid/bookingsystem/helpers/ObjectBuilder.java
@@ -18,5 +18,5 @@ public class ObjectBuilder {
         Long objectId = Long.parseLong(objectStrId);
         return objectId;
     }
-    
+
 }
--- Front-End/src/components/DailyTimeTable.js
+++ Front-End/src/components/DailyTimeTable.js
@@ -6,7 +6,8 @@ const DailyTimeTable = (props, NoBookingsPH) => {
 
   if(props.bookings.length === 0) return <p>No Bookings For this Date</p>;
 
-  const data = props.bookings.map(booking => {
+
+  const data = props.bookings.map((booking, index) => {
     return [booking.barber.name, booking.customer.name, new Date(booking.startTime), new Date(booking.endTime)]
   })
 
@@ -17,20 +18,13 @@ const DailyTimeTable = (props, NoBookingsPH) => {
       height={'200px'}
       chartType='Timeline'
       loader={<div>Loading Chart</div>}
-      data={[
-        [
-          { type: 'string', id: 'Barber' },
-          { type: 'string', id: 'Customer' },
-          { type: 'date', id: 'Start' },
-          { type: 'date', id: 'End' },
-        ],
-        ["Gemma", "Joe", new Date(0, 0, 0, 15, 0, 0), new Date(0, 0, 0, 15, 15, 0)],
-        ["Alan", "Steve", new Date(0, 0, 0, 12, 0, 0), new Date(0, 0, 0, 12, 30, 0)],
-        ["Jeff", "Jim", new Date(0, 0, 0, 10, 0, 0), new Date(0, 0, 0, 10, 15, 0)],
-        ["Jeff", "Tony", new Date(0, 0, 0, 9, 0, 0), new Date(0, 0, 0, 10, 0, 0)],
-        // {data}
-
+      columns={[
+        { type: 'string', id: 'Barber' },
+        { type: 'string', id: 'Customer' },
+        { type: 'date', id: 'Start' },
+        { type: 'date', id: 'End' }
       ]}
+      rows={data}
       options={{
         timeline: {
           singleColor: '#8d8',
